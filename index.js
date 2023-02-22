a3537278480fa049b454d201f9cac242fbcfc2b5
remble1 <pawel.sadkowski.1c@gmail.com>
2023-02-22 23:24:27 +0100

    back and front talks properly

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.post('/', (req, res) => {
+    res.json({
+        message: "Hello World!"
+    })
 })
 
 app.listen(port, () => {
     console.log('Example app listening');
-})
+});
---
+++ src/App.js
@@ -0,0 +1,36 @@
+// A express server, which will handle api request coming in and respond back with a json object, it will use body parser as well as corse
+import React, { useState } from "react";
+import './App.css';
+
+function App() {
+    const [message, setMessage] = useState('');
+    const [response, setResponse] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:3001/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ message }),
+        })
+        .then((res) => res.json())
+        .then((data) => setResponse(data.message));
+    };
+
+    return(
+        <div className="App">
+            <form onSubmit={handleSubmit}>
+                <textarea
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                ></textarea>
+                <button type="submit">Submit</button>            
+            </form>
+            <div>{response}</div>
+        </div>
+    );
+}
+
+export default App 
