9ea91296654d9257fa1dd0a8d659a55e74ef09a0
remble1 <pawel.sadkowski.1c@gmail.com>
2023-03-02 21:38:39 +0100

    core works fine, I get clear answear

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-// A express server, which will handle api request coming in and respond back with a json object, it will use body parser as well as corse
+// back
+const OpenAI = require('openai');
+const { Configuration, OpenAIApi } = OpenAI
+
 
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
 const port = 3001;
+require('dotenv').config()
+
+const configuration = new Configuration({
+    organization: "org-4b29loSrpWnPgqRFqvwApwEb",
+    apiKey: process.env.REACT_GPT_KEY,
+});
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
 
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/', (req, res) => {
-    res.json({
-        message: "Hello World!"
-    })
+app.post('/', async (req, res) => {
+    const { message } = req.body;
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `What is the best ${message} based on the opinions of reddit users. Answer in the form of a three element javascript list`,
+        max_tokens: 50,
+        temperature: 0,
+      });
+    console.log(response.data);
+    if(response.data.choices[0].text){
+        res.json({
+            message: response.data.choices[0].text})
+    }
 })
 
 app.listen(port, () => {
